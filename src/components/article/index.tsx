bee492783c66370b6b7c400972692708876f6839
ike <40803799+ikmnjrd@users.noreply.github.com>
2021-12-26 18:22:38 +0900

    記事フォーマットを関数化

--- src/components/article/index.tsx
+++ src/components/article/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { formatArticle } from '@/utils/article'
+
 import styles from './index.module.css'
 
 type Props = {
@@ -13,7 +15,7 @@ const Paragraph: React.FC<{ p: string }> = ({ p }) => {
 export const Article: React.FC<Props> = ({ content }) => {
   return (
     <>
-      {content.split('/n/n').map((p, i) => (
+      {formatArticle(content).map((p, i) => (
         <Paragraph p={p} key={i} />
       ))}
     </>
