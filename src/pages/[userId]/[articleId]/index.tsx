97097e92b59d3c07586dbced953d41cafd90be3c
ike <40803799+ikmnjrd@users.noreply.github.com>
2021-12-26 18:36:24 +0900

    記事ページのコンポーネント分割

--- src/pages/[userId]/[articleId]/index.tsx
+++ src/pages/[userId]/[articleId]/index.tsx
@@ -4,10 +4,44 @@ import Error from 'next/error'
 import styles from './index.module.css'
 import { Article } from '@/components/article'
 import { formatDate } from '@/utils/date'
-import { useGetArticleQuery } from '@/generated/graphql'
+import { useGetArticleQuery, Users } from '@/generated/graphql'
 
 import { SiteHeader } from '@/components/site-header'
 import { UserIcon } from '@/components/user-icon'
+
+type ArticleHeaderProps = {
+  subject: string
+  user: Pick<Users, 'display_id' | 'display_name'>
+  published_at: string
+}
+
+const ArticleHeader: React.FC<ArticleHeaderProps> = ({
+  subject,
+  user,
+  published_at,
+}) => {
+  const { datetime, isNew } = formatDate(new Date(published_at), new Date())
+  return (
+    <>
+      <h1 className={styles.subject}>{subject}</h1>
+
+      <div className={styles.userContainer}>
+        <div>
+          <UserIcon src="/profile.png" />
+        </div>
+        <div className={styles.userText}>
+          <div className={styles.userId}>
+            {user.display_name} @{user.display_id}
+          </div>
+          <span className={styles.publishedAt}>
+            <span>{datetime}</span>
+            {isNew ? <span className={styles.newContent}>New</span> : ''}
+          </span>
+        </div>
+      </div>
+    </>
+  )
+}
 
 const ArticlePage: NextPage = () => {
   const router = useRouter()
@@ -34,27 +68,14 @@ const ArticlePage: NextPage = () => {
   if (!published_at) {
     return <Error statusCode={404} />
   }
-  const { datetime, isNew } = formatDate(new Date(published_at), new Date())
+
+  const articleHeaderProps = { user, subject, published_at }
 
   return (
     <>
       <SiteHeader />
       <div className={styles.contentContainer}>
-        <h1 className={styles.subject}>{subject}</h1>
-        <div className={styles.userContainer}>
-          <div>
-            <UserIcon src="/profile.png" />
-          </div>
-          <div className={styles.userText}>
-            <div className={styles.userId}>
-              {user.display_name} @{user.display_id}
-            </div>
-            <span className={styles.published_at}>
-              <span>{datetime}</span>
-              {isNew ? <span className={styles.newContent}>New</span> : ''}
-            </span>
-          </div>
-        </div>
+        <ArticleHeader {...articleHeaderProps} />
         <div className={styles.content}>
           <Article content={content} />
         </div>
