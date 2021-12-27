4a3f6597db3a31f5bb933cf704fd55b9ad98c9ae
ike <40803799+ikmnjrd@users.noreply.github.com>
2021-12-27 16:28:50 +0900

    記事ヘッダと記事フッタを追加

--- src/pages/[userId]/[articleId]/index.tsx
+++ src/pages/[userId]/[articleId]/index.tsx
@@ -1,47 +1,14 @@
 import { useRouter } from 'next/router'
 import { NextPage } from 'next'
 import Error from 'next/error'
-import styles from './index.module.css'
+
+import { useGetArticleQuery } from '@/generated/graphql'
 import { Article } from '@/components/article'
-import { formatDate } from '@/utils/date'
-import { useGetArticleQuery, Users } from '@/generated/graphql'
-
+import { ArticleHeader } from '@/components/article/article-header'
+import { ArticleFooter } from '@/components/article/article-footer'
 import { SiteHeader } from '@/components/site-header'
-import { UserIcon } from '@/components/user-icon'
 
-type ArticleHeaderProps = {
-  subject: string
-  user: Pick<Users, 'display_id' | 'display_name'>
-  published_at: string
-}
-
-const ArticleHeader: React.FC<ArticleHeaderProps> = ({
-  subject,
-  user,
-  published_at,
-}) => {
-  const { datetime, isNew } = formatDate(new Date(published_at), new Date())
-  return (
-    <>
-      <h1 className={styles.subject}>{subject}</h1>
-
-      <div className={styles.userContainer}>
-        <div>
-          <UserIcon src="/profile.png" />
-        </div>
-        <div className={styles.userText}>
-          <div className={styles.userId}>
-            {user.display_name} @{user.display_id}
-          </div>
-          <span className={styles.publishedAt}>
-            <span>{datetime}</span>
-            {isNew ? <span className={styles.newContent}>New</span> : ''}
-          </span>
-        </div>
-      </div>
-    </>
-  )
-}
+import styles from './index.module.css'
 
 const ArticlePage: NextPage = () => {
   const router = useRouter()
@@ -79,6 +46,7 @@ const ArticlePage: NextPage = () => {
         <div className={styles.content}>
           <Article content={content} />
         </div>
+        <ArticleFooter user={user} />
       </div>
     </>
   )
